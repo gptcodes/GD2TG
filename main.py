229d3733c95bd7b785131e7ecce8494d76a5eb15
Dr. Metformin <76214556+anymeofu@users.noreply.github.com>
2023-03-22 19:55:31 +0530

    Add Error Handling

--- main.py
+++ main.py
@@ -30,6 +30,9 @@ async def download_file_from_drive(file_id):
     url = f'https://drive.google.com/u/0/uc?id={file_id}&export=download'
     async with aiohttp.ClientSession() as session:
         async with session.get(url) as response:
+            if response.status != 200:
+                raise Exception(f'Failed to download file {file_id}')
+
             content_disposition = response.headers.get('Content-Disposition')
             if content_disposition:
                 file_name = re.findall("filename=(.+)", content_disposition)[0].strip('"')
@@ -48,37 +51,44 @@ async def download_file_from_drive(file_id):
 
 @app.on_message(pyrogram.filters.command(['upload'], prefixes='/'))
 async def handle_upload_command(client, message):
-    # Get the Google Drive link from the message text
-    link = message.text.split()[1]
+    try:
+        # Get the Google Drive link from the message text
+        link = message.text.split()[1]
 
-    # Extract the folder ID and file ID from the link
-    folder_id = link.split('/')[-2]
-    file_id = link.split('/')[-1]
+        # Extract the folder ID and file ID from the link
+        folder_id = link.split('/')[-2]
+        file_id = link.split('/')[-1]
 
-    # Get a list of all file IDs in the folder
-    url = f'https://drive.google.com/drive/folders/{folder_id}'
-    async with aiohttp.ClientSession() as session:
-        async with session.get(url) as response:
-            html_text = await response.text()
-            ids = re.findall('/file/d/(.+?)"', html_text)
+        # Get a list of all file IDs in the folder
+        url = f'https://drive.google.com/drive/folders/{folder_id}'
+        async with aiohttp.ClientSession() as session:
+            async with session.get(url) as response:
+                if response.status != 200:
+                    raise Exception(f'Failed to get files from folder {folder_id}')
 
-    # Download the file and upload it to Telegram
-    file_name = await download_file_from_drive(file_id)
-    caption = os.path.splitext(file_name)[0]
-    await client.send_document(chat_id=message.chat.id, document=file_name, caption=caption)
+                html_text = await response.text()
+                ids = re.findall('/file/d/(.+?)"', html_text)
 
-    # Cleanup (optional)
-    os.remove(file_name)
+        # Download the file and upload it to Telegram
+        file_name = await download_file_from_drive(file_id)
+        caption = os.path.splitext(file_name)[0]
+        await client.send_document(chat_id=message.chat.id, document=file_name, caption=caption)
 
-    # Loop through all other files in the folder and repeat
-    for id in ids:
-        if id != file_id:
-            file_name = await download_file_from_drive(id)
-            caption = os.path.splitext(file_name)[0]
-            await client.send_document(chat_id=message.chat.id, document=file_name, caption=caption)
+        # Cleanup (optional)
+        os.remove(file_name)
 
-            # Cleanup (optional)
-            os.remove(file_name)
+        # Loop through all other files in the folder and repeat
+        for id in ids:
+            if id != file_id:
+                file_name = await download_file_from_drive(id)
+                caption = os.path.splitext(file_name)[0]
+                await client.send_document(chat_id=message.chat.id, document=file_name, caption=caption)
+
+                # Cleanup (optional)
+                os.remove(file_name)
+
+    except Exception as e:
+        await client.send_message(chat_id=message.chat.id, text=str(e))
 
 
 @app.on_message(pyrogram.filters.command(['start'], prefixes='/'))
